19ffbebf5f88ace5e77977ab6b7a60f3ec00015d
Isaac Dunn <gandalfthebignose@gmail.com>
2020-10-30 15:42:13 +0000

    Windows form task V0.1

---
+++ Week5/FormsWeek5/FormsWeek5/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormsWeek5
+{
+    public partial class Form1 : Form
+    {
+        public delegate void UpdateChatWindowDelegate(string messageReceived);
+        public UpdateChatWindowDelegate updateChatWindowDelegate;
+        Manager Manager;
+        public Form1(Manager manager)
+        {
+            Manager = manager;
+            InitializeComponent();
+            updateChatWindowDelegate += new UpdateChatWindowDelegate(UpdateChatWindow);
+        }
+
+        private void TerminalTemplate_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void UpdateChatWindow(string messageReceived)
+        {
+            if (MessageWindow.InvokeRequired)
+            {
+                Invoke(updateChatWindowDelegate, messageReceived);
+            }
+            else
+            {
+                MessageWindow.Text += messageReceived += "\n";
+                MessageWindow.SelectionStart = MessageWindow.Text.Length;
+                MessageWindow.ScrollToCaret();
+            }
+        }
+    }
+}
---
+++ Week5/FormsWeek5/FormsWeek5/Manager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace FormsWeek5
+{
+    public class Manager
+    {
+        Form1 m_Terminal1;
+        Form1 m_Terminal2;
+        public Manager()
+        {
+            m_Terminal1 = new Form1(this);
+            Thread FormThread1 = new Thread(() => { CreateForm(m_Terminal1); } );
+
+            m_Terminal2 = new Form1(this);
+            Thread FormThread2 = new Thread(() => { CreateForm(m_Terminal2); });
+
+            FormThread1.Start();
+            FormThread2.Start();
+
+            for (int i = 0; i < 10; i++)
+            {
+                SendMessageToAll("Message");
+                Thread.Sleep(500);
+            }
+
+            FormThread1.Join();
+            FormThread2.Join();
+        }
+
+        void CreateForm(Form1 terminal)
+        {
+            terminal.ShowDialog();
+        }
+
+        void SendMessageToAll(string message)
+        {
+            m_Terminal1.UpdateChatWindow(message);
+            m_Terminal2.UpdateChatWindow(message);
+        }
+    }
+}
