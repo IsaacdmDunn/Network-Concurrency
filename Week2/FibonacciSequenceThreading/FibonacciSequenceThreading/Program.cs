f5d264c0bf01e4eec0517ffb57cffe7a3ca37c1f
Isaac Dunn <gandalfthebignose@gmail.com>
2020-10-30 13:38:10 +0000

    Intro to threading
    Learning threading through coding with the fibonacci sequence

---
+++ Week2/FibonacciSequenceThreading/FibonacciSequenceThreading/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+
+namespace FibonacciSequenceThreading
+{
+    class Program
+    {
+        public static int a = 0;
+        public static int b = 1;
+
+        public static void Fibonacci()
+        {
+            
+            // In N steps compute Fibonacci sequence iteratively.
+            for (int i = 0; i < 10; i++)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+                Console.WriteLine(a);
+            }
+        }
+        static void Main()
+        {
+            
+            Thread thread1 = new Thread(new ThreadStart(() => Program.Fibonacci()));
+            Thread thread2 = new Thread(new ThreadStart(() => Program.Fibonacci()));
+            Thread thread3 = new Thread(new ThreadStart(() => Program.Fibonacci()));
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+            thread1.Join();
+            thread2.Join();
+            thread3.Join();
+
+            for (int i = 0; i < 30; i++)
+            {
+                //Console.WriteLine(Fibonacci(i));
+            }
+        }
+    }
+}
---
+++ Week2/Threading/Threading/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Threading;
+using System.Transactions;
+
+namespace Threading
+{
+    class Program
+    {
+        public static int number = 0;
+        public static int threadCount = 1;
+
+        public static void DoWork(object _i)
+        {
+            if (Convert.ToInt32(_i) < 500)
+            {
+                for (int i = Convert.ToInt32(_i); i <= Convert.ToInt32(_i) + 10; i++)
+                {
+                    Console.WriteLine(i);
+                }
+                Thread.Sleep(100);
+                number += 10;
+
+                _i = number;
+
+                Thread thread1 = new Thread(DoWork);
+                Thread thread2 = new Thread(DoWork);
+                thread1.Start(_i);
+                thread2.Start(_i);
+                threadCount += 2;
+                Console.WriteLine("threadCount:------------" + threadCount);
+            }
+        }
+    }
+
+    class ThreadTest
+    {
+        public static void Main()
+        {
+            int i = 0;
+            Thread threads = new Thread(Program.DoWork);
+            threads.Start(i);
+        }
+    }
+}
+
+
+//namespace Threading
+//{
+//    class Program
+//    {
+//        public static int threadCount = 1;
+
+//        public static void DoWork(int _i)
+//        {
+//            if (Program.threadCount < 250)
+//            {
+
+
+//                for (int i = _i; i < _i + 10; i++)
+//                {
+//                    Console.WriteLine(i);
+//                    Thread.Sleep(100);
+//                }
+//                _i += 10;
+
+//                Thread thread1 = new Thread(() => Program.DoWork(_i));
+//                Thread thread2 = new Thread(() => Program.DoWork(_i));
+//                thread1.Start();
+//                thread2.Start();
+//                threadCount += 2;
+//                Console.WriteLine("threadCount:------------" + threadCount);
+//            }
+//        }
+//    }
+
+//    class ThreadTest
+//    {
+//        public static void Main()
+//        {
+//            int i = 0;
+//            Thread threads = new Thread(() => Program.DoWork(i));
+//            threads.Start();
+//        }
+
+//    }
+//}
